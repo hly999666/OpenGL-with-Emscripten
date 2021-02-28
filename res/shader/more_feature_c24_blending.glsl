7450194e44e1907cf0c7e4dfc4b63c07ac154dcb
hly <hly920928@gmail.com>
2021-02-28 15:55:35 +0800

    Discard and Alpha Blending

---
+++ res/shader/more_feature_c24_blending.glsl
@@ -0,0 +1,39 @@
+//shader vertex
+#version 300 es
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    TexCoords = aTexCoords;
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
+//shader fragment
+#version 300 es
+precision highp float;
+
+uniform sampler2D tex_1;
+uniform int mode;
+uniform float discard_threshold;
+in vec2 TexCoords;
+
+out vec4 FragColor;
+
+void main()
+{
+    vec4 texColor = texture(tex_1, TexCoords);
+    if(mode==1){
+       if(texColor.a < discard_threshold){
+           discard;
+          
+       }
+   
+    }
+    FragColor = texColor;
+}
---
+++ src/main_gl_c24_Blending.cpp
@@ -0,0 +1,438 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+#include <GL/glew.h>     
+#endif
+#include <GLFW/glfw3.h>
+#include <functional>
+#include <iostream>
+#include<string>
+#include <vector>
+#include <map>
+#ifdef __EMSCRIPTEN__
+#define GLM_FORCE_SIMD_AVX 
+#else
+#define GLM_FORCE_AVX2  
+#endif
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp" 
+#include "gtc/type_ptr.hpp" 
+
+#ifndef GL_HELPER_H
+#include "gl_helper.hpp"
+#include "gl_geometry.hpp"
+#endif
+#ifndef SHADER_H
+#include  "gl_shading.hpp"
+#endif
+#ifndef GL_MESH_H
+#include  "gl_mesh.hpp"
+#endif
+#include  "gl_ui.hpp"
+ 
+#include  "Optr_ol.hpp"
+ 
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <chrono>
+
+ 
+
+
+//main render loop
+std::function<void()> render_loop;
+void main_loop() { render_loop(); }
+//ui callback
+std::function<void(GLFWwindow* window, int width, int height)>frame_buffer_resize;
+void on_frame_buffer_resize(GLFWwindow* window, int width, int height) {
+	frame_buffer_resize(window, width, height);
+};
+ 
+//std::function<void(GLFWwindow* window, int width, int height)> on_framebuffer_size_callback;
+std::function<void(GLFWwindow* window, double xpos, double ypos)> mouse_callback;
+void on_mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	mouse_callback(window, xpos, ypos);
+};
+std::function<void(GLFWwindow* window, double xoffset, double yoffset)> scroll_callback;
+void on_scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	scroll_callback(window, xoffset, yoffset);
+};
+std::function<void(GLFWwindow* window)> processInput;
+ 
+int main()
+
+{
+
+	  int width = 1280;
+	  int height = 720;
+	//for camera and UI
+		//camera
+	lyh_gl::helper::Camera camera(glm::vec3(1.0f, 0.0f, 4.0f));
+	float lastX = width / 2.0f;
+	float lastY = height / 2.0f;
+	bool firstMouse = true;
+
+	// timing
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
+	const char* glsl_version = "#version 300 es";
+
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+ 
+
+
+	GLFWwindow* window = lyh_gl::helper::gl_init(width, height,"Blending");
+
+	//set up UI
+	{
+		frame_buffer_resize = [&width, &height](GLFWwindow* window, int _width, int _height)->void {
+			width = _width; height = _height;
+			glViewport(0, 0, width, height);
+
+		};
+		glfwSetFramebufferSizeCallback(window, on_frame_buffer_resize);
+		mouse_callback = [&](GLFWwindow* window, double xpos, double ypos)
+		{
+
+
+
+			if (firstMouse)
+			{
+				lastX = xpos;
+				lastY = ypos;
+				firstMouse = false;
+			}
+
+			float xoffset = xpos - lastX;
+			float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+			lastX = xpos;
+			lastY = ypos;
+			int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE);
+			if (state != GLFW_PRESS)return;
+			camera.ProcessMouseMovement(xoffset, yoffset);
+		};
+		glfwSetCursorPosCallback(window, on_mouse_callback);
+		/*scroll_callback = [&](GLFWwindow* window, double xoffset, double yoffset)
+		{
+			camera.ProcessMouseScroll(yoffset);
+		};
+		glfwSetScrollCallback(window, on_scroll_callback);*/
+		processInput = [&deltaTime, &camera](GLFWwindow* _window)->void
+		{
+			using namespace lyh_gl::helper;
+			if (glfwGetKey(_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(_window, true);
+			}
+
+
+			if (glfwGetKey(_window, GLFW_KEY_W) == GLFW_PRESS) {
+				//std::cout << "Press_W" << std::endl;
+				camera.ProcessKeyboard(FORWARD, deltaTime);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_S) == GLFW_PRESS) {
+				//std::cout << "Press_S" << std::endl;
+				camera.ProcessKeyboard(BACKWARD, deltaTime);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_A) == GLFW_PRESS) {
+				//std::cout << "Press_A" << std::endl;
+				camera.ProcessKeyboard(LEFT, deltaTime);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_D) == GLFW_PRESS) {
+				//std::cout << "Press_D" << std::endl;
+				camera.ProcessKeyboard(RIGHT, deltaTime);
+			}
+
+		};
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
+
+
+	//global opengl setting
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//create shader
+ 
+
+	lyh_gl::shading::Shader blending_shader("more_feature_c24_blending.glsl");
+ 
+
+	//build geometry
+	unsigned int cubeVAO, cubeVBO;
+	unsigned int planeVAO, planeVBO;
+	unsigned int transparentVAO, transparentVBO;
+	{
+	
+	
+		float cubeVertices[] = {
+			// positions          // texture Coords
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		};
+		float planeVertices[] = {
+			// positions          // texture Coords 
+			 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+			-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+			-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+			 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+			-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+			 5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+		};
+		float transparentVertices[] = {
+			// positions         // texture Coords (swapped y coordinates because texture is flipped upside down)
+			0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+			0.0f, -0.5f,  0.0f,  0.0f,  1.0f,
+			1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+
+			0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+			1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+			1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+		};
+		// cube VAO
+		 
+		glGenVertexArrays(1, &cubeVAO);
+		glGenBuffers(1, &cubeVBO);
+		glBindVertexArray(cubeVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		// plane VAO
+		 
+		glGenVertexArrays(1, &planeVAO);
+		glGenBuffers(1, &planeVBO);
+		glBindVertexArray(planeVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		// transparent VAO
+	 
+		glGenVertexArrays(1, &transparentVAO);
+		glGenBuffers(1, &transparentVBO);
+		glBindVertexArray(transparentVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		glBindVertexArray(0);
+	
+
+	
+	}
+
+
+
+
+	auto marble_diffuse = std::make_shared<lyh_gl::helper::gl_texture>("texture/marble.jpg", "async_joinable", GL_RGB, "tex_1", false);
+	auto metal_diffuse = std::make_shared<lyh_gl::helper::gl_texture>("texture/metal.jpg", "async_joinable", GL_RGB, "tex_1", false);
+	auto window_tex = std::make_shared<lyh_gl::helper::gl_texture>("texture/window.png", "async_joinable", GL_RGB, "tex_1", false);
+	//tex_diffuse->loading_thread_join_blocking();
+	
+	//meshes[0].textures.push_back(tex_diffuse);
+	//lightingShader.setTexUnit(*tex_diffuse);
+	marble_diffuse->loading_thread_join_blocking();
+	metal_diffuse->loading_thread_join_blocking();
+	window_tex->loading_thread_join_blocking();
+	blending_shader.setTexUnit(*marble_diffuse);
+	blending_shader.setTexUnit(*metal_diffuse);
+	blending_shader.setTexUnit(*window_tex);
+	//set up ui
+
+
+	std::vector<glm::vec3> windows
+	{
+		glm::vec3(-1.5f, 0.0f, -0.48f),
+		glm::vec3(1.5f, 0.0f, 0.51f),
+		glm::vec3(0.0f, 0.0f, 0.7f),
+		glm::vec3(-0.3f, 0.0f, -2.3f),
+		glm::vec3(0.5f, 0.0f, -0.6f)
+	};
+
+
+	lyh_gl::ui::set_up(window, glsl_version);
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	auto font_1 = io.Fonts->AddFontFromFileTTF("./res/font/Roboto-Medium.ttf", 16.0f);
+	  
+
+	blending_shader.use();
+	int mode = 1;
+	float discard_threshold = 0.3;
+	render_loop = [&] {
+		//timing
+		float currentFrame = glfwGetTime();
+		glfwMakeContextCurrent(window);
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+	    processInput(window);
+		 
+	
+		
+		//setup viewport and basic rendering state
+		
+		glEnable(GL_DEPTH_TEST);
+		
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glViewport(0, 0, width, height);
+
+
+		//set up GUI
+		
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::PushFont(font_1);
+
+
+		ImGui::SetNextWindowPos({ 32,50 });
+		ImGui::SetNextWindowSize({ 360,120 });
+		ImGui::Begin("Alpha Mode");
+		 
+		ImGui::RadioButton("discard", &mode,1); 
+		ImGui::RadioButton("blending", &mode, 2);  
+	 
+
+		ImGui::SliderFloat("discard threshold", &discard_threshold, 0.0, 1.0);
+		ImGui::End();
+
+		ImGui::PopFont();
+		ImGui::Render();
+ 
+		//set uniform 
+		 
+	
+	
+	 
+
+		//sort window by distance using std::map
+		std::map<float, glm::vec3> sorted;
+		for (unsigned int i = 0; i < windows.size(); i++)
+		{
+			float distance = glm::length(camera.Position - windows[i]);
+			sorted[distance] = windows[i];
+		}
+
+
+		blending_shader.use();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+		blending_shader.setMat4("projection", projection);
+		blending_shader.setMat4("view", view);
+		blending_shader.setInt("mode", mode);
+		blending_shader.setFloat("discard_threshold", discard_threshold);
+
+ 
+
+		glBindVertexArray(cubeVAO);
+		blending_shader.bindTex("tex_1",*marble_diffuse);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		blending_shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+ 
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, 0.0f));
+		blending_shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		 
+		glBindVertexArray(planeVAO);
+		blending_shader.bindTex("tex_1", *metal_diffuse);
+		model = glm::mat4(1.0f);
+		blending_shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+		glBindVertexArray(transparentVAO);
+		blending_shader.bindTex("tex_1", *window_tex);
+
+		for (auto it = sorted.rbegin(); it != sorted.rend(); ++it)
+		{
+			 
+			model = glm::translate(glm::mat4(1.0f), it->second);
+			blending_shader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	};
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(main_loop, 0, true);
+#else
+	while (!glfwWindowShouldClose(window))main_loop();
+#endif
+
+
+	// Cleanup
+	//cleanup imgui
+	//glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &VBO);
+
+	//cleanup glfw
+	lyh_gl::ui::clear_up();
+	lyh_gl::helper::gl_clear_up(window);
+
+	return 0;
+	
+};
+
+ 
