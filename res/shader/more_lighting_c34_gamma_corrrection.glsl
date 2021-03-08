0c951cc6401a85b7fa103192f081bc6616ba6d0c
hly <hly920928@gmail.com>
2021-03-08 09:29:07 +0800

    Apply gamma correction

---
+++ res/shader/more_lighting_c34_gamma_corrrection.glsl
@@ -0,0 +1,79 @@
+//shader vertex
+#version 300 es
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} vs_out;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+void main()
+{
+    vs_out.FragPos = aPos;
+    vs_out.Normal = aNormal;
+    vs_out.TexCoords = aTexCoords;
+    gl_Position = projection * view * vec4(aPos, 1.0);
+}
+//shader fragment
+#version 300 es
+precision highp float;
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} fs_in;
+
+uniform sampler2D floorTexture;
+
+uniform vec3 lightPositions[4];
+uniform vec3 lightColors[4];
+uniform float light_factor[4];
+uniform vec3 viewPos;
+uniform bool input_gamma;
+uniform bool output_gamma;
+
+vec3 BlinnPhong(vec3 normal, vec3 fragPos, vec3 lightPos, vec3 lightColor)
+{
+    // diffuse
+    vec3 lightDir = normalize(lightPos - fragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
+    // specular
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;    
+    // simple attenuation
+    float max_distance = 1.5;
+    float distance = length(lightPos - fragPos);
+    float attenuation = 1.0 / (output_gamma ? distance * distance : distance);
+    
+    diffuse *= attenuation;
+    specular *= attenuation;
+    
+    return diffuse + specular;
+}
+
+void main()
+{           
+    vec3 color = texture(floorTexture, fs_in.TexCoords).rgb;
+    if(input_gamma)color = pow(color, vec3(1.0/2.2));
+    vec3 lighting = vec3(0.0);
+    for(int i = 0; i < 4; ++i){
+         lighting +=light_factor[i]*BlinnPhong(normalize(fs_in.Normal), fs_in.FragPos, lightPositions[i], lightColors[i]);
+    };
+    color *= lighting;
+    if(output_gamma)color = pow(color, vec3(1.0/2.2));
+        
+    FragColor = vec4(color, 1.0);
+}
---
+++ src/main_gl_c34_gamma_corrrection.cpp
@@ -0,0 +1,301 @@
+﻿#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+#include <GL/glew.h>     
+#endif
+#include <GLFW/glfw3.h>
+#include <functional>
+#include <iostream>
+#include<string>
+#include <vector>
+#include <map>
+#ifdef __EMSCRIPTEN__
+#define GLM_FORCE_SIMD_AVX 
+#else
+#define GLM_FORCE_AVX2  
+#endif
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp" 
+#include "gtc/type_ptr.hpp" 
+
+#ifndef GL_HELPER_H
+#include "gl_helper.hpp"
+#include "gl_geometry.hpp"
+#endif
+#ifndef SHADER_H
+#include  "gl_shading.hpp"
+#endif
+#ifndef GL_MESH_H
+#include  "gl_mesh.hpp"
+#endif
+#include  "gl_ui.hpp"
+ 
+#include  "Optr_ol.hpp"
+ 
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <chrono>
+
+ 
+
+
+//main render loop
+std::function<void()> render_loop;
+void main_loop() { render_loop(); }
+//ui callback
+std::function<void(GLFWwindow* window, int width, int height)>frame_buffer_resize;
+void on_frame_buffer_resize(GLFWwindow* window, int width, int height) {
+	frame_buffer_resize(window, width, height);
+};
+ 
+//std::function<void(GLFWwindow* window, int width, int height)> on_framebuffer_size_callback;
+std::function<void(GLFWwindow* window, double xpos, double ypos)> mouse_callback;
+void on_mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	mouse_callback(window, xpos, ypos);
+};
+std::function<void(GLFWwindow* window, double xoffset, double yoffset)> scroll_callback;
+void on_scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	scroll_callback(window, xoffset, yoffset);
+};
+std::function<void(GLFWwindow* window)> processInput;
+ 
+int main()
+
+{
+
+	  int width = 1280;
+	  int height = 720;
+	//for camera and UI
+		//camera
+	lyh_gl::helper::Camera camera(glm::vec3(0.0f, 0.0f, 6.0f));
+	float lastX = width / 2.0f;
+	float lastY = height / 2.0f;
+	bool firstMouse = true;
+	float movingSpeed = 1.0;
+	// timing
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
+	const char* glsl_version = "#version 300 es";
+
+	glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+     
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	GLFWwindow* window = lyh_gl::helper::gl_init(width, height,"gamma correction");
+
+	//set up UI
+	{
+		frame_buffer_resize = [&width, &height](GLFWwindow* window, int _width, int _height)->void {
+			width = _width; height = _height;
+			glViewport(0, 0, width, height);
+
+		};
+		glfwSetFramebufferSizeCallback(window, on_frame_buffer_resize);
+		mouse_callback = [&](GLFWwindow* window, double xpos, double ypos)
+		{
+
+
+
+			if (firstMouse)
+			{
+				lastX = xpos;
+				lastY = ypos;
+				firstMouse = false;
+			}
+
+			float xoffset = xpos - lastX;
+			float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+			lastX = xpos;
+			lastY = ypos;
+			int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE);
+			if (state != GLFW_PRESS)return;
+			camera.ProcessMouseMovement(xoffset, yoffset);
+		};
+		glfwSetCursorPosCallback(window, on_mouse_callback);
+		/*scroll_callback = [&](GLFWwindow* window, double xoffset, double yoffset)
+		{
+			camera.ProcessMouseScroll(yoffset);
+		};
+		glfwSetScrollCallback(window, on_scroll_callback);*/
+		processInput = [&deltaTime, &camera, movingSpeed](GLFWwindow* _window)->void
+		{
+			using namespace lyh_gl::helper;
+			if (glfwGetKey(_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(_window, true);
+			}
+
+
+			if (glfwGetKey(_window, GLFW_KEY_W) == GLFW_PRESS) {
+				//std::cout << "Press_W" << std::endl;
+				camera.ProcessKeyboard(FORWARD, deltaTime* movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_S) == GLFW_PRESS) {
+				//std::cout << "Press_S" << std::endl;
+				camera.ProcessKeyboard(BACKWARD, deltaTime * movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_A) == GLFW_PRESS) {
+				//std::cout << "Press_A" << std::endl;
+				camera.ProcessKeyboard(LEFT, deltaTime * movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_D) == GLFW_PRESS) {
+				//std::cout << "Press_D" << std::endl;
+				camera.ProcessKeyboard(RIGHT, deltaTime * movingSpeed);
+			}
+
+		};
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
+
+ 
+	//create shader
+ 
+	lyh_gl::shading::Shader blinn_phong_shader("more_lighting_c33_Blinn-Phong.glsl");
+	//load model and texture
+	lyh_gl::shading::Shader gamma_corrrection_shader("more_lighting_c34_gamma_corrrection.glsl");
+	auto floorTexture = std::make_shared<lyh_gl::helper::gl_texture>("texture/wood.jpg", "async_joinable", GL_RGB, "floorTexture", false);
+	floorTexture->loading_thread_join_blocking();
+	blinn_phong_shader.use();
+	blinn_phong_shader.setInt("floorTexture", 0);
+ 
+	gamma_corrrection_shader.use();
+	gamma_corrrection_shader.setInt("floorTexture", 0);
+
+	unsigned int planeVAO, planeVBO;
+	{
+		float planeVertices[] = {
+			// positions            // normals         // texcoords
+			 10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+			-10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+			-10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+
+			 10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+			-10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+			 10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
+		};
+		// plane VAO
+	
+		glGenVertexArrays(1, &planeVAO);
+		glGenBuffers(1, &planeVBO);
+		glBindVertexArray(planeVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindVertexArray(0);
+	}
+ 
+	// lighting info
+// -------------
+	glm::vec3 lightPositions[] = {
+		glm::vec3(-3.0f, 0.0f, 0.0f),
+		glm::vec3(-1.0f, 0.0f, 0.0f),
+		glm::vec3(1.0f, 0.0f, 0.0f),
+		glm::vec3(3.0f, 0.0f, 0.0f)
+	};
+	glm::vec3 lightColors[] = {
+		glm::vec3(0.25),
+		glm::vec3(0.50),
+		glm::vec3(0.75),
+		glm::vec3(1.00)
+	};
+//set up ui
+	lyh_gl::ui::set_up(window, glsl_version);
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	auto font_1 = io.Fonts->AddFontFromFileTTF("./res/font/Roboto-Medium.ttf", 16.0f);
+	  
+	 
+	bool output_gamma = true;
+	bool input_gamma = false;
+	float light_factor[4] = {1.0,1.0,1.0,1.0};
+	render_loop = [&] {
+		//timing
+		float currentFrame = glfwGetTime();
+		glfwMakeContextCurrent(window);
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+	    processInput(window);
+	
+		glEnable(GL_DEPTH_TEST);
+ 
+
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		 //imGui
+		{
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			ImGui::PushFont(font_1);
+
+
+			ImGui::SetNextWindowPos({ 32,50 });
+			ImGui::SetNextWindowSize({ 300,120 });
+			ImGui::Begin("Input");
+			ImGui::Checkbox("Render Output Gamma",&output_gamma);
+			ImGui::Checkbox("Texture Input Gamma", &input_gamma);
+			ImGui::SliderFloat4("Light mixer", light_factor,0,2.0);
+			ImGui::End();
+
+			ImGui::PopFont();
+			ImGui::Render();
+		}
+
+		gamma_corrrection_shader.use();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		gamma_corrrection_shader.setMat4("projection", projection);
+		gamma_corrrection_shader.setMat4("view", view);
+		// set light uniforms,4 vec3 once
+		glUniform3fv(glGetUniformLocation(gamma_corrrection_shader.ID, "lightPositions"), 4, &lightPositions[0][0]);
+		glUniform3fv(glGetUniformLocation(gamma_corrrection_shader.ID, "lightColors"), 4, &lightColors[0][0]);
+		glUniform1fv(glGetUniformLocation(gamma_corrrection_shader.ID, "light_factor"), 4, light_factor);
+		gamma_corrrection_shader.setVec3("viewPos", camera.Position);
+		gamma_corrrection_shader.setInt("output_gamma", output_gamma);
+		gamma_corrrection_shader.setInt("input_gamma", input_gamma);
+		// floor
+		glBindVertexArray(planeVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D,floorTexture->texture_id);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	};
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(main_loop, 0, true);
+#else
+	while (!glfwWindowShouldClose(window))main_loop();
+#endif
+
+	 
+
+	//cleanup glfw
+	lyh_gl::ui::clear_up();
+	lyh_gl::helper::gl_clear_up(window);
+
+	return 0;
+	
+};
+
+ 
