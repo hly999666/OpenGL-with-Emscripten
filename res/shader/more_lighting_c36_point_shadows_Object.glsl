d5c453772127a9da927f642a94024b36bb6fd83e
hly <hly920928@gmail.com>
2021-03-10 16:26:49 +0800

    point shadow by rendering to cubemap

---
+++ res/shader/more_lighting_c36_point_shadows_Object.glsl
@@ -0,0 +1,123 @@
+//shader vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+
+out VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} vs_out;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+uniform int reverse_normals;
+
+void main()
+{
+    vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
+    //reverse normal for outside cube
+    if(reverse_normals==1){
+          vs_out.Normal = transpose(inverse(mat3(model))) * (-1.0 * aNormal);
+    }else{
+          vs_out.Normal = transpose(inverse(mat3(model))) * aNormal;
+    };     
+    vs_out.TexCoords = aTexCoords;
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
+//shader fragment
+#version 330 core
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+} fs_in;
+
+uniform sampler2D diffuseTexture;
+uniform samplerCube depthMap;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+uniform float far_plane;
+uniform bool shadows;
+uniform int mode;
+uniform float diskRadius_factor;
+const int samples = 20;
+
+// array of offset direction for sampling
+vec3 gridSamplingDisk[20] = vec3[]
+(
+   vec3(1, 1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1, 1,  1), 
+   vec3(1, 1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1, 1, -1),
+   vec3(1, 1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1, 1,  0),
+   vec3(1, 0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1, 0, -1),
+   vec3(0, 1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0, 1, -1)
+);
+
+float ShadowCalculation(vec3 fragPos)
+{
+    
+    vec3 fragToLight = fragPos - lightPos;
+    
+    float currentDepth = length(fragToLight);
+   
+    float shadow = 0.0;
+    float bias = 0.15;
+   
+    float viewDistance = length(viewPos - fragPos);
+    float diskRadius = (1.0 + (viewDistance / far_plane)) / 25.0;
+    for(int i = 0; i < samples; ++i)
+    {
+        float closestDepth = texture(depthMap, fragToLight + gridSamplingDisk[i] * diskRadius*diskRadius_factor).r;
+        closestDepth *= far_plane;   
+        if(currentDepth - bias > closestDepth)
+            shadow += 1.0;
+    }
+    shadow /= float(samples);
+        
+    // display closestDepth as debug (to visualize depth cubemap)
+    // FragColor = vec4(vec3(closestDepth / far_plane), 1.0);    
+        
+    return shadow;
+}
+
+void main()
+{           
+    if(mode==1){
+         vec3 fragToLight = fs_in.FragPos - lightPos;
+    
+        float closestDepth = texture(depthMap, fragToLight).r;
+        FragColor = vec4(vec3(closestDepth), 1.0);  
+    }else{
+    vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 normal = normalize(fs_in.Normal);
+    vec3 lightColor = vec3(0.3);
+    // ambient
+    vec3 ambient = 0.3 * color;
+    // diffuse
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
+    // specular
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;    
+    // calculate shadow
+    float shadow = shadows ? ShadowCalculation(fs_in.FragPos) : 0.0;                      
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
+    
+    FragColor = vec4(lighting, 1.0);
+    }
+  
+}
---
+++ src/main_gl_c36_point_shadows.cpp
@@ -0,0 +1,521 @@
+﻿#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+#include <GL/glew.h>     
+#endif
+#include <GLFW/glfw3.h>
+#include <functional>
+#include <iostream>
+#include<string>
+#include <vector>
+#include <map>
+#ifdef __EMSCRIPTEN__
+#define GLM_FORCE_SIMD_AVX 
+#else
+#define GLM_FORCE_AVX2  
+#endif
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp" 
+#include "gtc/type_ptr.hpp" 
+
+#ifndef GL_HELPER_H
+#include "gl_helper.hpp"
+#include "gl_geometry.hpp"
+#endif
+#ifndef SHADER_H
+#include  "gl_shading.hpp"
+#endif
+#ifndef GL_MESH_H
+#include  "gl_mesh.hpp"
+#endif
+#include  "gl_ui.hpp"
+ 
+#include  "Optr_ol.hpp"
+ 
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <chrono>
+
+ 
+
+
+//main render loop
+std::function<void()> render_loop;
+void main_loop() { render_loop(); }
+//ui callback
+std::function<void(GLFWwindow* window, int width, int height)>frame_buffer_resize;
+void on_frame_buffer_resize(GLFWwindow* window, int width, int height) {
+	frame_buffer_resize(window, width, height);
+};
+ 
+//std::function<void(GLFWwindow* window, int width, int height)> on_framebuffer_size_callback;
+std::function<void(GLFWwindow* window, double xpos, double ypos)> mouse_callback;
+void on_mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	mouse_callback(window, xpos, ypos);
+};
+std::function<void(GLFWwindow* window, double xoffset, double yoffset)> scroll_callback;
+void on_scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	scroll_callback(window, xoffset, yoffset);
+};
+std::function<void(GLFWwindow* window)> processInput;
+ 
+int main()
+
+{
+
+	  int width = 1280;
+	  int height = 720;
+	 
+	//for camera and UI
+	//camera
+	lyh_gl::helper::Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+	float lastX = width / 2.0f;
+	float lastY = height / 2.0f;
+	bool firstMouse = true;
+	float movingSpeed = 1.0;
+	// timing
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
+	const char* glsl_version = "#version 330 core";
+
+ 
+     
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	GLFWwindow* window = lyh_gl::helper::gl_init(width, height,"point shadow");
+
+	//set up UI
+	{
+		frame_buffer_resize = [&width, &height](GLFWwindow* window, int _width, int _height)->void {
+			width = _width; height = _height;
+			glViewport(0, 0, width, height);
+
+		};
+		glfwSetFramebufferSizeCallback(window, on_frame_buffer_resize);
+		mouse_callback = [&](GLFWwindow* window, double xpos, double ypos)
+		{
+
+
+
+			if (firstMouse)
+			{
+				lastX = xpos;
+				lastY = ypos;
+				firstMouse = false;
+			}
+
+			float xoffset = xpos - lastX;
+			float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+			lastX = xpos;
+			lastY = ypos;
+			int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE);
+			if (state != GLFW_PRESS)return;
+			camera.ProcessMouseMovement(xoffset, yoffset);
+		};
+		glfwSetCursorPosCallback(window, on_mouse_callback);
+		/*scroll_callback = [&](GLFWwindow* window, double xoffset, double yoffset)
+		{
+			camera.ProcessMouseScroll(yoffset);
+		};
+		glfwSetScrollCallback(window, on_scroll_callback);*/
+		processInput = [&deltaTime, &camera, movingSpeed](GLFWwindow* _window)->void
+		{
+			using namespace lyh_gl::helper;
+			if (glfwGetKey(_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(_window, true);
+			}
+
+
+			if (glfwGetKey(_window, GLFW_KEY_W) == GLFW_PRESS) {
+				//std::cout << "Press_W" << std::endl;
+				camera.ProcessKeyboard(FORWARD, deltaTime* movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_S) == GLFW_PRESS) {
+				//std::cout << "Press_S" << std::endl;
+				camera.ProcessKeyboard(BACKWARD, deltaTime * movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_A) == GLFW_PRESS) {
+				//std::cout << "Press_A" << std::endl;
+				camera.ProcessKeyboard(LEFT, deltaTime * movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_D) == GLFW_PRESS) {
+				//std::cout << "Press_D" << std::endl;
+				camera.ProcessKeyboard(RIGHT, deltaTime * movingSpeed);
+			}
+
+		};
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
+
+ 
+	//create shader
+ 
+ 
+	lyh_gl::shading::Shader depth_cube_shader("more_lighting_c36_point_shadows_depth_cube.glsl");
+	lyh_gl::shading::Shader point_shadow_object("more_lighting_c36_point_shadows_Object.glsl");
+	//load model and texture
+	bool shadows = true;
+	bool shadowsKeyPressed = false;
+
+	//set up geometry
+	unsigned int planeVAO, planeVBO;
+	{
+		float planeVertices[] = {
+			// positions            // normals         // texcoords
+			 25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+			-25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+			-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+			 25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+			-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+			 25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+		};
+		// plane VAO
+	 
+		glGenVertexArrays(1, &planeVAO);
+		glGenBuffers(1, &planeVBO);
+		glBindVertexArray(planeVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindVertexArray(0);
+
+	}
+
+	unsigned int cubeVAO, cubeVBO;
+	{
+		float vertices[] = {
+			// back face
+			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+			 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+			 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+			 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+			-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+			// front face
+			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+			 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+			 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+			 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+			-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+			// left face
+			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+			-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+			-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+			// right face
+			 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+			 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+			 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+			 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+			 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+			 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+			// bottom face
+			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+			 1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+			 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+			 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+			-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+			// top face
+			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+			 1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+			 1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+			 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+			-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
+		};
+		glGenVertexArrays(1, &cubeVAO);
+		glGenBuffers(1, &cubeVBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(cubeVAO);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	} 
+	unsigned int quadVAO; unsigned int quadVBO;
+	{
+		float quadVertices[] = {
+			// positions        // texture Coords
+			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		};
+		// setup plane VAO
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	}
+	//set shadow map
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	unsigned int depthMapFBO;unsigned int depthCubemap;
+	{
+		
+		const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	 
+		glGenFramebuffers(1, &depthMapFBO);
+		// create depth cubemap texture
+	
+		glGenTextures(1, &depthCubemap);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubemap);
+		for (unsigned int i = 0; i < 6; ++i) {
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 
+				                      0, 
+				                     GL_DEPTH_COMPONENT, 
+				                     SHADOW_WIDTH, 
+				                     SHADOW_HEIGHT,
+				                     0, 
+				                     GL_DEPTH_COMPONENT, 
+				                     GL_FLOAT,
+				                     NULL
+			                         );
+		}
+			
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		// attach depth texture as FBO's depth buffer
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthCubemap, 0);
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	auto floorTexture = std::make_shared<lyh_gl::helper::gl_texture>("texture/wood.jpg", "async_joinable", GL_RGB, "diffuseTexture", false);
+	floorTexture->loading_thread_join_blocking();
+
+
+ 
+
+
+	point_shadow_object.use();
+	point_shadow_object.setInt("diffuseTexture", 0);
+	point_shadow_object.setInt("depthMap", 1);
+	// lighting info
+	
+
+//set up ui
+	lyh_gl::ui::set_up(window, glsl_version);
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	auto font_1 = io.Fonts->AddFontFromFileTTF("./res/font/Roboto-Medium.ttf", 16.0f);
+	   
+	glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+	float near_plane = 1.0f;float far_plane = 25.0f;
+	float shadow_space_size = 10.0f;
+	float shadow_radius = 1.0;
+	bool debug = false;
+	int light_mode = 1;
+	float fovy = 90.0;
+	//helper function for rendering
+	auto renderCube = [&]() {
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+	};
+	auto renderQuad = [&]() {
+		glBindVertexArray(quadVAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
+	};
+	auto renderScene = [&](lyh_gl::shading::Shader& shader, bool reverse_normal = true) {
+		// room cube
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::scale(model, glm::vec3(5.0f));
+		shader.setMat4("model", model);
+		//avoid cull face because of wind order
+		glDisable(GL_CULL_FACE); 
+        //reverse normal for correct rendering
+		if(reverse_normal)shader.setInt("reverse_normals", 1);
+		renderCube();
+		if (reverse_normal)shader.setInt("reverse_normals", 0);
+		glEnable(GL_CULL_FACE);
+		// cubes
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(4.0f, -3.5f, 0.0));
+		model = glm::scale(model, glm::vec3(0.5f));
+		shader.setMat4("model", model);
+		renderCube();
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 3.0f, 1.0));
+		model = glm::scale(model, glm::vec3(0.75f));
+		shader.setMat4("model", model);
+		renderCube();
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-3.0f, -1.0f, 0.0));
+		model = glm::scale(model, glm::vec3(0.5f));
+		shader.setMat4("model", model);
+		renderCube();
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.5f, 1.0f, 1.5));
+		model = glm::scale(model, glm::vec3(0.5f));
+		shader.setMat4("model", model);
+		renderCube();
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.5f, 2.0f, -3.0));
+		model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0)));
+		model = glm::scale(model, glm::vec3(0.75f));
+		shader.setMat4("model", model);
+		renderCube();
+
+
+	};
+	render_loop = [&] {
+		//timing
+		float currentFrame = glfwGetTime();
+		glfwMakeContextCurrent(window);
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+	    processInput(window);
+	
+		glEnable(GL_DEPTH_TEST);
+ 
+
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		//moving light
+		lightPos.z = sin(glfwGetTime() * 0.5) * 3.0;
+		 //imGui
+		{
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			ImGui::PushFont(font_1);
+
+
+			ImGui::SetNextWindowPos({ 32,50 });
+			ImGui::SetNextWindowSize({ 300,120 });
+			ImGui::Begin("Input");
+			ImGui::Checkbox("Render Debug",&debug);
+			ImGui::SliderFloat3("Light Position", &lightPos.x, -10.0, 10.0); 
+			ImGui::SliderFloat("Shadow Radius Factor", &shadow_radius,0.0, 2.0);
+			/*ImGui::Checkbox("Texture Inpu,t Gamma", &input_gamma);
+			ImGui::SliderFloat4("Light mixer", light_factor,0,2.0);*/
+			ImGui::End();
+
+			ImGui::PopFont();
+			ImGui::Render();
+		}
+		 
+
+		//pass 1 render depth
+		{
+			glm::mat4 shadowProj = glm::perspective(glm::radians(90.0f), (float)SHADOW_WIDTH / (float)SHADOW_HEIGHT, near_plane, far_plane);
+			std::vector<glm::mat4> shadowTransforms;
+			shadowTransforms.push_back(shadowProj* glm::lookAt(lightPos, lightPos + glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f)));
+			shadowTransforms.push_back(shadowProj* glm::lookAt(lightPos, lightPos + glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f)));
+			shadowTransforms.push_back(shadowProj* glm::lookAt(lightPos, lightPos + glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)));
+			shadowTransforms.push_back(shadowProj* glm::lookAt(lightPos, lightPos + glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f)));
+			shadowTransforms.push_back(shadowProj* glm::lookAt(lightPos, lightPos + glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, -1.0f, 0.0f)));
+			shadowTransforms.push_back(shadowProj* glm::lookAt(lightPos, lightPos + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f)));
+
+			glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+			glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+			glClear(GL_DEPTH_BUFFER_BIT);
+			depth_cube_shader.use();
+			for (unsigned int i = 0; i < 6; ++i)
+				depth_cube_shader.setMat4("shadowMatrices[" + std::to_string(i) + "]", shadowTransforms[i]);
+			depth_cube_shader.setFloat("far_plane", far_plane);
+			depth_cube_shader.setVec3("lightPos", lightPos);
+			renderScene(depth_cube_shader,false);
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		}
+	 //pass 2 render scene with shadow map
+       {
+			// reset viewport first !!!!!
+			glViewport(0, 0, width, height);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			point_shadow_object.use();
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, 0.1f, 100.0f);
+			glm::mat4 view = camera.GetViewMatrix();
+			point_shadow_object.setMat4("projection", projection);
+			point_shadow_object.setMat4("view", view);
+			// set lighting uniforms
+			point_shadow_object.setVec3("lightPos", lightPos);
+			point_shadow_object.setVec3("viewPos", camera.Position);
+			point_shadow_object.setInt("shadows", shadows); 
+			point_shadow_object.setInt("mode", debug);
+			point_shadow_object.setFloat("far_plane", far_plane);
+			point_shadow_object.setFloat("diskRadius_factor", shadow_radius);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, floorTexture->texture_id);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubemap);
+			renderScene(point_shadow_object);
+		
+		
+		
+		}
+
+	  //debug pass 
+		/*if (debug) {
+			debug_quad_shader.use();
+			debug_quad_shader.setInt("mode", 0);
+			debug_quad_shader.setFloat("near_plane", near_plane);
+			debug_quad_shader.setFloat("far_plane", far_plane);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, depthMap);
+			renderQuad();
+		}*/
+
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	};
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(main_loop, 0, true);
+#else
+	while (!glfwWindowShouldClose(window))main_loop();
+#endif
+
+	 
+
+	//cleanup glfw
+	lyh_gl::ui::clear_up();
+	lyh_gl::helper::gl_clear_up(window);
+
+	return 0;
+	
+};
+
+ 
