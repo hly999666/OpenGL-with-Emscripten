665cd244bcbf19cd309bfaaf57d0126dcb4e4ffa
hly <hly920928@gmail.com>
2021-03-17 21:54:56 +0800

    Bloom Effect : Shader

---
+++ src/main_gl_c40_Bloom.cpp
@@ -0,0 +1,449 @@
+﻿#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+#include <GL/glew.h>     
+#endif
+#include <GLFW/glfw3.h>
+#include <functional>
+#include <iostream>
+#include<string>
+#include <vector>
+#include <map>
+#ifdef __EMSCRIPTEN__
+#define GLM_FORCE_SIMD_AVX 
+#else
+#define GLM_FORCE_AVX2  
+#endif
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp" 
+#include "gtc/type_ptr.hpp" 
+
+#ifndef GL_HELPER_H
+#include "gl_helper.hpp"
+#include "gl_geometry.hpp"
+#endif
+#ifndef SHADER_H
+#include  "gl_shading.hpp"
+#endif
+#ifndef GL_MESH_H
+#include  "gl_mesh.hpp"
+#endif
+#include  "gl_ui.hpp"
+ 
+#include  "Optr_ol.hpp"
+ 
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <chrono>
+
+ 
+
+
+//main render loop
+std::function<void()> render_loop;
+void main_loop() { render_loop(); }
+//ui callback
+std::function<void(GLFWwindow* window, int width, int height)>frame_buffer_resize;
+void on_frame_buffer_resize(GLFWwindow* window, int width, int height) {
+	frame_buffer_resize(window, width, height);
+};
+ 
+//std::function<void(GLFWwindow* window, int width, int height)> on_framebuffer_size_callback;
+std::function<void(GLFWwindow* window, double xpos, double ypos)> mouse_callback;
+void on_mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	mouse_callback(window, xpos, ypos);
+};
+std::function<void(GLFWwindow* window, double xoffset, double yoffset)> scroll_callback;
+void on_scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	scroll_callback(window, xoffset, yoffset);
+};
+std::function<void(GLFWwindow* window)> processInput;
+ 
+int main()
+
+{
+
+	  int width = 1280;
+	  int height = 720;
+	 
+	//for camera and UI
+	//camera
+	lyh_gl::helper::Camera camera(glm::vec3(0.0f, 0.0f, -2.0f));
+	camera.Pitch= 180.0f;
+	camera.updateCameraVectors();
+	float lastX = width / 2.0f;
+	float lastY = height / 2.0f;
+	bool firstMouse = true;
+	float movingSpeed = 1.0;
+	// timing
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
+	const char* glsl_version = "#version 330 core";
+
+ 
+     
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	GLFWwindow* window = lyh_gl::helper::gl_init(width, height,"HDR");
+
+	//set up UI
+	{
+		frame_buffer_resize = [&width, &height](GLFWwindow* window, int _width, int _height)->void {
+			width = _width; height = _height;
+			glViewport(0, 0, width, height);
+
+		};
+		glfwSetFramebufferSizeCallback(window, on_frame_buffer_resize);
+		mouse_callback = [&](GLFWwindow* window, double xpos, double ypos)
+		{
+
+
+
+			if (firstMouse)
+			{
+				lastX = xpos;
+				lastY = ypos;
+				firstMouse = false;
+			}
+
+			float xoffset = xpos - lastX;
+			float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+			lastX = xpos;
+			lastY = ypos;
+			int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE);
+			if (state != GLFW_PRESS)return;
+			camera.ProcessMouseMovement(xoffset, yoffset);
+		};
+		glfwSetCursorPosCallback(window, on_mouse_callback);
+		/*scroll_callback = [&](GLFWwindow* window, double xoffset, double yoffset)
+		{
+			camera.ProcessMouseScroll(yoffset);
+		};
+		glfwSetScrollCallback(window, on_scroll_callback);*/
+		processInput = [&deltaTime, &camera, movingSpeed](GLFWwindow* _window)->void
+		{
+			using namespace lyh_gl::helper;
+			if (glfwGetKey(_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+				glfwSetWindowShouldClose(_window, true);
+			}
+
+
+			if (glfwGetKey(_window, GLFW_KEY_W) == GLFW_PRESS) {
+				//std::cout << "Press_W" << std::endl;
+				camera.ProcessKeyboard(FORWARD, deltaTime* movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_S) == GLFW_PRESS) {
+				//std::cout << "Press_S" << std::endl;
+				camera.ProcessKeyboard(BACKWARD, deltaTime * movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_A) == GLFW_PRESS) {
+				//std::cout << "Press_A" << std::endl;
+				camera.ProcessKeyboard(LEFT, deltaTime * movingSpeed);
+			}
+
+			if (glfwGetKey(_window, GLFW_KEY_D) == GLFW_PRESS) {
+				//std::cout << "Press_D" << std::endl;
+				camera.ProcessKeyboard(RIGHT, deltaTime * movingSpeed);
+			}
+
+		};
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
+
+ 
+	//create shader
+    lyh_gl::shading::Shader lighting_shader("more_lighting_c39_HDR_lighting.glsl");
+
+	lyh_gl::shading::Shader tonemapping_shader("more_lighting_c39_HDR_tonemapping.glsl");
+
+	lyh_gl::shading::Shader bloom_object("more_lighting_c40_bloom_object.glsl");
+	lyh_gl::shading::Shader bloom_light("more_lighting_c40_bloom_light.glsl");
+	lyh_gl::shading::Shader bloom_blur("more_lighting_c40_bloom_blur.glsl");
+	lyh_gl::shading::Shader bloom_final("more_lighting_c40_bloom_final.glsl");
+	//load model and texture
+ 
+	auto wood_texture = std::make_shared<lyh_gl::helper::gl_texture>("texture/wood.jpg", "async_joinable", GL_RGB, "diffuseMap", false);
+	//set up floating point fbo
+	unsigned int hdrFBO;	unsigned int colorBuffer;
+	{
+	
+		glGenFramebuffers(1, &hdrFBO);
+		// create floating point color buffer
+	
+		glGenTextures(1, &colorBuffer);
+		glBindTexture(GL_TEXTURE_2D, colorBuffer);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// create depth buffer (renderbuffer)
+		unsigned int rboDepth;
+		glGenRenderbuffers(1, &rboDepth);
+		glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+		// attach buffers
+		glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "Framebuffer not complete!" << std::endl;
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+	//light info
+
+	std::vector<glm::vec3> lightPositions;
+	std::vector<glm::vec3> lightColors;
+	{
+		
+		lightPositions.push_back(glm::vec3(0.0f, 0.0f, 49.5f)); // back light
+		lightPositions.push_back(glm::vec3(-1.4f, -1.9f, 9.0f));
+		lightPositions.push_back(glm::vec3(0.0f, -1.8f, 4.0f));
+		lightPositions.push_back(glm::vec3(0.8f, -1.7f, 6.0f));
+		// colors
+	
+		lightColors.push_back(glm::vec3(200.0f, 200.0f, 200.0f));
+		lightColors.push_back(glm::vec3(0.1f, 0.0f, 0.0f));
+		lightColors.push_back(glm::vec3(0.0f, 0.0f, 0.2f));
+		lightColors.push_back(glm::vec3(0.0f, 0.1f, 0.0f));
+	}
+	wood_texture->loading_thread_join_blocking();
+
+	lighting_shader.use();
+	lighting_shader.setInt("diffuseTexture", 0);
+
+	tonemapping_shader.use();
+	tonemapping_shader.setInt("hdrBuffer", 0);
+
+
+	//set up geometry
+	unsigned int cubeVAO = 0;
+	unsigned int cubeVBO = 0;
+	 
+	{
+		// initialize (if necessary)
+		 
+		float vertices[] = {
+			// back face
+			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+			 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+			 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+			 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+			-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+			// front face
+			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+			 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+			 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+			 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+			-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+			// left face
+			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+			-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+			-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+			// right face
+			 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+			 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+			 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+			 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+			 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+			 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+			// bottom face
+			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+			 1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+			 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+			 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+			-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+			// top face
+			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+			 1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+			 1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+			 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+			-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
+		};
+			glGenVertexArrays(1, &cubeVAO);
+			glGenBuffers(1, &cubeVBO);
+			// fill buffer
+			glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			// link vertex attributes
+			glBindVertexArray(cubeVAO);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+			glEnableVertexAttribArray(2);
+			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+};
+	auto renderCube = [&]() {
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+	};
+
+	unsigned int quadVAO;
+	unsigned int quadVBO;
+ 
+	{
+		float quadVertices[] = {
+			// positions        // texture Coords
+			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		};
+		// setup plane VAO
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	};
+
+	auto renderQuad = [&]() {
+		glBindVertexArray(quadVAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
+	};
+//set up ui
+	lyh_gl::ui::set_up(window, glsl_version);
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	auto font_1 = io.Fonts->AddFontFromFileTTF("./res/font/Roboto-Medium.ttf", 16.0f);
+	   
+    
+	int  hdr_mode = 1;
+	float exposure = 1.0f;
+	float main_light_intense = 200.0;
+	render_loop = [&] {
+		//timing
+		float currentFrame = glfwGetTime();
+		glfwMakeContextCurrent(window);
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+	    processInput(window);
+	
+		glEnable(GL_DEPTH_TEST);
+ 
+
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		//moving light
+		 
+		 //imGui
+		{
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			ImGui::PushFont(font_1);
+
+
+			ImGui::SetNextWindowPos({ 32,50 });
+			ImGui::SetNextWindowSize({ 300,200 });
+			ImGui::Begin("Input");
+			ImGui::Text("HDR mode");
+			ImGui::RadioButton("None", &hdr_mode, 0);
+			ImGui::RadioButton("Reinhard", &hdr_mode,1);
+			ImGui::RadioButton("Exposure", &hdr_mode, 2);
+			ImGui::SliderFloat("ev", &exposure, 0.0, 10.0);
+			ImGui::SliderFloat("main light intense", &main_light_intense, 0.0, 10000.0);
+		/*	ImGui::SliderFloat3("Light Position", &lightPos.x, -10.0, 10.0);
+			ImGui::Text("Parallax Mode");
+			ImGui::RadioButton("Basic",&parallax_mode,0);
+			ImGui::RadioButton("Steep", &parallax_mode,1);
+			ImGui::RadioButton("Occlusion", &parallax_mode, 2);
+			ImGui::SliderFloat("Height Scale", &heightScale,0.0,1.0);*/
+			
+			//ImGui::SliderFloat("Normal Intense", &normal_intense,0.0, 2.0);
+			/*ImGui::Checkbox("Texture Inpu,t Gamma", &input_gamma);
+			ImGui::SliderFloat4("Light mixer", light_factor,0,2.0);*/
+			ImGui::End();
+
+			ImGui::PopFont();
+			ImGui::Render();
+		}
+		lightColors[0] = glm::vec3(main_light_intense, main_light_intense, main_light_intense);
+		 //pass 1,render float-point framebuffer
+		{
+			auto& cam = camera;
+			glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+			glm::mat4 view = camera.GetViewMatrix();
+			lighting_shader.use();
+			lighting_shader.setMat4("projection", projection);
+			lighting_shader.setMat4("view", view);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, wood_texture->texture_id);
+			// set lighting uniforms
+			auto& _lightpos = lightPositions;
+			for (unsigned int i = 0; i < _lightpos.size(); i++)
+			{
+				lighting_shader.setVec3("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
+				lighting_shader.setVec3("lights[" + std::to_string(i) + "].Color", lightColors[i]);
+			}
+			//lighting_shader.setVec3("viewPos", camera.Position);
+			// render tunnel
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(0.0f, 0.0f, 25.0));
+			model = glm::scale(model, glm::vec3(2.5f, 2.5f, 27.5f));
+			lighting_shader.setMat4("model", model);
+			lighting_shader.setInt("inverse_normals", true);
+			renderCube();
+		
+		}
+		//pass 2,render with tone-mapping
+		{
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			tonemapping_shader.use();
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, colorBuffer);
+			tonemapping_shader.setInt("hdr_mode", hdr_mode);
+			tonemapping_shader.setFloat("exposure", exposure);
+			renderQuad();
+		}
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	};
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(main_loop, 0, true);
+#else
+	while (!glfwWindowShouldClose(window))main_loop();
+#endif
+
+	 
+
+	//cleanup glfw
+	lyh_gl::ui::clear_up();
+	lyh_gl::helper::gl_clear_up(window);
+
+	return 0;
+	
+};
+
+ 
